79934cb17c10a557e8b1d6b4742f4baaa76f817c
Paksh <pakshgupta007@gmail.com>
2024-04-10 17:31:45 +0530

    Connect Database

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Use import dotenv
+
+Change in packet.json 
+
+-r dotenv/config --experimental-json-modules wiht the help of this we an use import dotennv instead of require(dotenv)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-    
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:'./env'
+})
+connectDB();
+
+
+
+
+/* 
+This is the first approach for connecting backend 
+
+import express from 'express';
+const app=express();
+(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{      // This is to check if our express app is getting problem in connecting
+            console.log("Error:",error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening at port number,${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.error("Error is :",error);
+        throw error;
+    }
+})()
+*/
