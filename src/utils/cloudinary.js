b47dfa09c3964ab0a811545acb74e62d2aa9ce2b
Paksh <pakshgupta007@gmail.com>
2024-04-12 11:29:18 +0530

    add upload file functionality and its middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; // node.js file system tp perform CRUD operation on a file
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudnary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      console.log("File path not found");
+      return null;
+    }
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("File uploaded successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally save temperory file as the upload operation got failed
+    // becaouse we do not want the currept file present in our server
+    return null;
+  }
+};
+
+export {uploadOnCloudnary}
