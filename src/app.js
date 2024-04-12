42e04b9cbf7722221a67cca7e3d17e370a917fba
Paksh <pakshgupta007@gmail.com>
2024-04-12 11:27:13 +0530

    fix bug

--- src/app.js
+++ src/app.js
@@ -12,5 +12,5 @@ app.use(cors(corsOptions))
 app.use(express.json({limit:"16kb"}))  // accept json with a limit of 16kb so that my server do not crash
 app.use(express.urlencoded({extended:true,limit:"16kb"}))  // to configure url in some url space can be trated as %20 in some + so to configure it we use this
 app.use(express.static("public"))  // static folders which any one can access.
-app.use(express.cookieParser()) // server s user k browser ki cookies ko access krke unpe CRUD operation perform kr paye
+app.use(cookieParser()) // server s user k browser ki cookies ko access krke unpe CRUD operation perform kr paye
 export {app}
