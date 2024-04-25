f6929b8e1d0c30d9bd59a87947a4c0b66c7e8219
Paksh <pakshgupta007@gmail.com>
2024-04-25 23:35:25 +0530

    add healthcare,like and tweet controllers

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,113 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const alreadyLiked = await Like.findOne({
+    video: videoId,
+    likedBy: req.user?._id,
+  });
+
+  if (alreadyLiked) {
+    await Like.findByIdAndDelete(alreadyLiked?._id);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { isLiked: false }, "Video unliked successfully")
+      );
+  }
+
+  const liked = await Like.create({
+    video: videoId,
+    likedBy: req.user?._id,
+  });
+
+  if (!liked) {
+    throw new ApiError(500, "Server error not able to like video");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, liked, "Video liked successfully"));
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid commentId");
+  }
+  const alreadyCommented = await Like.findOne({
+    commen: commentId,
+    likedBy: req.use?._id,
+  });
 
-})
+  if (alreadyCommented) {
+    await Like.findByIdAndDelete(alreadyCommented?._id);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { isLiked: false }, "Comment unliked successfully")
+      );
+  }
+
+  const commentLike = await Like.create({
+    comment: commentId,
+    likedBy: req.user?._id,
+  });
+
+  if (!commentLike) {
+    throw new ApiError(500, "Server error not able to dislike");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, commentLike, "Comment unliked successfully"));
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  const { tweetId } = req.params;
+  if (!isValidObjectId(tweetIdId)) {
+    throw new ApiError(400, "Invalid tweetId");
+  }
+
+  const alreadyLiked = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user?._id,
+  });
+
+  if (alreadyLiked) {
+    await Like.findByIdAndDelete(alreadyLiked?._id);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { isLiked: false }, "Tweet unliked successfully")
+      );
+  }
+
+  const liked = await Like.create({
+    video: tweetId,
+    likedBy: req.user?._id,
+  });
+
+  if (!liked) {
+    throw new ApiError(500, "Server error not able to like tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, liked, "Tweet liked successfully"));
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+  //TODO: get all liked videos
+});
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,13 +1,41 @@
 import mongoose, { isValidObjectId } from "mongoose";
-import { User } from "../models/user.model.js";
-import { Subscription } from "../models/subscription.model.js";
+import { Subscription } from "../models/subscription.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  // TODO: toggle subscription
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channelId");
+  }
+
+  const isSubscribed = await Subscription.findOne({
+    subscribe: req.user?._id,
+    channel: channelId,
+  });
+
+  if (isSubscribed) {
+    await Subscription.findByIdAndDelete(alreadyLiked?._id);
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { isSubscribed: false }, "Unsubscribe successfully")
+      );
+  }
+
+  const subscribing = await Like.create({
+    subscribe: req.user?._id,
+    channelId: channelId,
+  });
+
+  if (!subscribing) {
+    throw new ApiError(500, "Server error not able to subscribe");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, subscribing , "Subscribe successfully"));
 });
 
 // controller to return subscriber list of a channel
@@ -39,3 +67,5 @@ const getSubscribedChannels = asyncHandler(async (req, res) => {
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
+
+
